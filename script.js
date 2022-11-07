0856b1737c8d68fc2f3ad763c07553fa08824d44
Sarinha09 <saradutra98765@gmail.com>
2022-11-07 19:27:54 -0300

    Gerador

---
+++ script.js
@@ -0,0 +1,34 @@
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!$@&';
+let novaSenha = '';
+
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function() {
+  sizePassword.innerHTML = this.value;
+}
+
+
+function generatePassword(){
+
+  let pass = '';
+  for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+    pass += charset.charAt(Math.floor(Math.random() * n));
+  }
+  
+  console.log(pass)
+  password.innerHTML = pass;
+  novaSenha = pass;
+}
+
+function copyPassword(){
+  alert("Senha copiada com sucesso!")
+  navigator.clipboard.writeText(novaSenha);
+}
